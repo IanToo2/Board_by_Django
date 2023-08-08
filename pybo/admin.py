65f61bfcd2ec122523a357e5b77938d5926390be
KimJungIn <rlawjddla0203@gmail.com>
2023-08-08 22:10:25 +0900

    관리자 페이지 수정

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import Question
-# Register your models here.
+from . import models
+
+class AnswerInline(admin.TabularInline):
+    model = models.Answer
+    extra = 0
 
 class QuestionAdmin(admin.ModelAdmin):
-    search_fileds = ['subject']
+    list_display = ('author', 'modify_date', 'subject', 'content', 'create_date')
+    list_filter=['subject']
+    fieldsets = [
+        (None, {'fields': ['author']}),
+        ('제목 내용 !!!', {'fields': ['subject']}),
+    ]
+    inlines = [AnswerInline]
+    
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('author', 'modify_date', 'question', 'content', 'create_date')
+    list_filter=['content']
+    fieldsets = [
+        (None, {'fields': ['author']}),
+        ('답글 내용 !!!', {'fields': ['content']}),
+    ]
 
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.Answer, AnswerAdmin)
