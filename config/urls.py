9f384c2f6e486a6e08f7483b441161ea455b8329
KimJungIn <rlawjddla0203@gmail.com>
2023-08-08 21:37:03 +0900

    url 주소 변경 그러나 실패

--- config/urls.py
+++ config/urls.py
@@ -61,5 +61,5 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('api-auth/',include('rest_framework.urls'))
+    path('restapi/api-auth/',include('rest_framework.urls')),
 ]
